2dcf0d09927e28e7dcb928f98ad9da777dee6e45
fjconcha527-glitch <fjconcha527@gmail.com>
2026-04-24 15:50:44 +0200

    update staging models with explicit column lists

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -3,5 +3,15 @@ select
     first_name,
     last_name,
     email,
-    country
+    phone,
+    age,
+    gender,
+    country,
+    state,
+    city,
+    registration_date,
+    customer_segment,
+    total_orders,
+    total_spent,
+    is_active
 from {{ source('raw', 'customers') }}
