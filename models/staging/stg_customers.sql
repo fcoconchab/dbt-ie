1512681377be596ae7d5bd131630c1f8eb4b0268
fjconcha527-glitch <fjconcha527@gmail.com>
2026-04-24 17:11:29 +0200

    add segments seed and build int_customers intermediate model

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -3,15 +3,6 @@ select
     first_name,
     last_name,
     email,
-    phone,
-    age,
-    gender,
     country,
-    state,
-    city,
-    registration_date,
-    customer_segment,
-    total_orders,
-    total_spent,
-    is_active
+    customer_segment
 from {{ source('raw', 'customers') }}
